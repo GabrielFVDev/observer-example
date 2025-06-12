54247003d90e4d3c02efdee67b7889de14c10187
Gabriel FV <gabrielvasco906@gmail.com>
2025-06-12 16:00:58 -0300

    refactor: Simplify BottomSheetService and update TestePage to use showModal

--- lib/app/view/teste_page.dart
+++ lib/app/view/teste_page.dart
@@ -1,40 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/app/service/bottom_sheet_service.dart';
-import 'package:flutter_application_3/app/widgets/verification_step_card.dart';
 
 class TestePage extends StatelessWidget {
   const TestePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    void _openCamera() {
-      // Sua lógica para abrir a câmera
-      print('Abrindo câmera...');
-    }
-
-    void _openGallery() {
-      // Sua lógica para abrir galeria
-      print('Abrindo galeria...');
-    }
-
-    return ElevatedButton(
-      onPressed: () async {
-        final result = await BottomSheetService.showDocumentPicker(context);
-
-        if (result != null) {
-          switch (result) {
-            case 'camera':
-              // Lógica para abrir câmera
-              _openCamera();
-              break;
-            case 'gallery':
-              // Lógica para escolher documento da galeria
-              _openGallery();
-              break;
-          }
-        }
-      },
-      child: Text('Anexar Documento'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Teste')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await BottomSheetService.showModal(
+              context: context,
+              builder: (context) => const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Conteúdo do BottomSheet'),
+              ),
+            );
+          },
+          child: const Text('Anexar Documento'),
+        ),
+      ),
     );
   }
 }
